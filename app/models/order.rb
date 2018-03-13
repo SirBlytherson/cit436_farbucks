a5c225ad831d141ad950b68ffeb6901e84aed513
blrbrown <blrbrown@iupui.edu>
2018-03-13 16:10:58 +0000

    Up to using order item form

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   belongs_to :location
   belongs_to :status
   has_many :order_items
+
+  before_validation :set_order_status_location, on: :create
+
+  private 
+  def set_order_status_location
+    self.status_id = 1
+    self.location_id = 1
+  end
 end
